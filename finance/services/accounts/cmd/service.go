06d61b9428bd6150d69377340a40a562c22c41f6
Caleb Meyer <calebzmeyer@gmail.com>
2023-12-18 10:12:44 -0800

    Add ListTransactions Skeleton (#21)

--- finance/services/accounts/cmd/service.go
+++ finance/services/accounts/cmd/service.go
@@ -15,3 +15,7 @@ func NewAccountsService() *AccountsService {
 func (service *AccountsService) CreateTransaction(ctx context.Context, request *accounts.CreateTransactionRequest) (*accounts.CreateTransactionResponse, error) {
 	return &accounts.CreateTransactionResponse{}, nil
 }
+
+func (service *AccountsService) ListTransactions(ctx context.Context, request *accounts.ListTransactionsRequest) (*accounts.ListTransactionsResponse, error) {
+	return &accounts.ListTransactionsResponse{}, nil
+}
