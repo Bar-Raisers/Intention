9bbb656c56dfcd27dae296e1ff01d1c141236159
Caleb Meyer <calebzmeyer@gmail.com>
2024-01-09 02:05:27 +0000

    Fix Command Line Args for ServiceFlags

--- common/services/flags.go
+++ common/services/flags.go
@@ -19,17 +19,13 @@ func NewServiceFlags() *ServiceFlags {
 		fs: flag.NewFlagSet("service_flags", flag.PanicOnError),
 	}
 
-	flag.StringVar(&serviceFlags.address, "address", defaultIpAddress, "service ip address")
-	flag.IntVar(&serviceFlags.port, "port", defaultPort, "service port")
+	serviceFlags.fs.StringVar(&serviceFlags.address, "address", defaultIpAddress, "service ip address")
+	serviceFlags.fs.IntVar(&serviceFlags.port, "port", defaultPort, "service port")
 
 	return serviceFlags
 }
 
 func (s *ServiceFlags) Init(args []string) error {
-	if args == nil {
-		args = []string{"address", "port"}
-	}
-
 	return s.fs.Parse(args)
 }
 
