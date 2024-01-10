cf9397529fbf81c56fe43ecd55d49d331c242b60
Caleb Meyer <calebzmeyer@gmail.com>
2024-01-10 14:29:12 -0800

    Initialize AccountsService Database Connection (#27)

--- finance/services/accounts/cmd/service.go
+++ finance/services/accounts/cmd/service.go
@@ -3,13 +3,30 @@ package main
 import (
 	"context"
 
+	"gorm.io/gorm"
+
+	"github.com/bar-raisers/intention/common/resources/db"
 	accounts "github.com/bar-raisers/intention/finance/contracts/accounts/v1"
 )
 
-type AccountsService struct{}
+type AccountsService struct {
+	db *gorm.DB
+}
 
 func NewAccountsService() *AccountsService {
-	return &AccountsService{}
+	dataSource := db.NewDataSource(
+		"localhost",
+		"postgres",
+		"postgres",
+		"db",
+		5432,
+		"America/Vancouver",
+	)
+	db := db.Connect(dataSource)
+
+	return &AccountsService{
+		db: db,
+	}
 }
 
 func (service *AccountsService) CreateTransaction(ctx context.Context, request *accounts.CreateTransactionRequest) (*accounts.CreateTransactionResponse, error) {
