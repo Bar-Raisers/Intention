00c5fbb4145228f794528d997f09ffe2329620e4
Caleb Meyer <calebzmeyer@gmail.com>
2024-03-09 23:57:20 +0000

    Extend AccountsService Contract for Update & Delete

--- contracts/finance/accounts/v1/transaction.pb.go
+++ contracts/finance/accounts/v1/transaction.pb.go
@@ -106,6 +106,91 @@ func (*CreateTransactionResponse) Descriptor() ([]byte, []int) {
 	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{1}
 }
 
+type DeleteTransactionRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	TransactionId uint32 `protobuf:"varint,1,opt,name=transaction_id,json=transactionId,proto3" json:"transaction_id,omitempty"`
+}
+
+func (x *DeleteTransactionRequest) Reset() {
+	*x = DeleteTransactionRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *DeleteTransactionRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*DeleteTransactionRequest) ProtoMessage() {}
+
+func (x *DeleteTransactionRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use DeleteTransactionRequest.ProtoReflect.Descriptor instead.
+func (*DeleteTransactionRequest) Descriptor() ([]byte, []int) {
+	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *DeleteTransactionRequest) GetTransactionId() uint32 {
+	if x != nil {
+		return x.TransactionId
+	}
+	return 0
+}
+
+type DeleteTransactionResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *DeleteTransactionResponse) Reset() {
+	*x = DeleteTransactionResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *DeleteTransactionResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*DeleteTransactionResponse) ProtoMessage() {}
+
+func (x *DeleteTransactionResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use DeleteTransactionResponse.ProtoReflect.Descriptor instead.
+func (*DeleteTransactionResponse) Descriptor() ([]byte, []int) {
+	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{3}
+}
+
 type ListTransactionsRequest struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -115,7 +200,7 @@ type ListTransactionsRequest struct {
 func (x *ListTransactionsRequest) Reset() {
 	*x = ListTransactionsRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[2]
+		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[4]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -128,7 +213,7 @@ func (x *ListTransactionsRequest) String() string {
 func (*ListTransactionsRequest) ProtoMessage() {}
 
 func (x *ListTransactionsRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[2]
+	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[4]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -141,7 +226,7 @@ func (x *ListTransactionsRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ListTransactionsRequest.ProtoReflect.Descriptor instead.
 func (*ListTransactionsRequest) Descriptor() ([]byte, []int) {
-	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{2}
+	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{4}
 }
 
 type ListTransactionsResponse struct {
@@ -155,7 +240,7 @@ type ListTransactionsResponse struct {
 func (x *ListTransactionsResponse) Reset() {
 	*x = ListTransactionsResponse{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[3]
+		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[5]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -168,7 +253,7 @@ func (x *ListTransactionsResponse) String() string {
 func (*ListTransactionsResponse) ProtoMessage() {}
 
 func (x *ListTransactionsResponse) ProtoReflect() protoreflect.Message {
-	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[3]
+	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[5]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -181,12 +266,106 @@ func (x *ListTransactionsResponse) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ListTransactionsResponse.ProtoReflect.Descriptor instead.
 func (*ListTransactionsResponse) Descriptor() ([]byte, []int) {
-	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{3}
+	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{5}
 }
 
 func (x *ListTransactionsResponse) GetTransactions() []*finance.Transaction {
 	if x != nil {
 		return x.Transactions
+	}
+	return nil
+}
+
+type UpdateTransactionRequest struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Transaction *finance.Transaction `protobuf:"bytes,1,opt,name=transaction,proto3" json:"transaction,omitempty"`
+}
+
+func (x *UpdateTransactionRequest) Reset() {
+	*x = UpdateTransactionRequest{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateTransactionRequest) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateTransactionRequest) ProtoMessage() {}
+
+func (x *UpdateTransactionRequest) ProtoReflect() protoreflect.Message {
+	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateTransactionRequest.ProtoReflect.Descriptor instead.
+func (*UpdateTransactionRequest) Descriptor() ([]byte, []int) {
+	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *UpdateTransactionRequest) GetTransaction() *finance.Transaction {
+	if x != nil {
+		return x.Transaction
+	}
+	return nil
+}
+
+type UpdateTransactionResponse struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Transaction *finance.Transaction `protobuf:"bytes,1,opt,name=transaction,proto3" json:"transaction,omitempty"`
+}
+
+func (x *UpdateTransactionResponse) Reset() {
+	*x = UpdateTransactionResponse{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[7]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *UpdateTransactionResponse) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*UpdateTransactionResponse) ProtoMessage() {}
+
+func (x *UpdateTransactionResponse) ProtoReflect() protoreflect.Message {
+	mi := &file_contracts_finance_accounts_v1_transaction_proto_msgTypes[7]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use UpdateTransactionResponse.ProtoReflect.Descriptor instead.
+func (*UpdateTransactionResponse) Descriptor() ([]byte, []int) {
+	return file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP(), []int{7}
+}
+
+func (x *UpdateTransactionResponse) GetTransaction() *finance.Transaction {
+	if x != nil {
+		return x.Transaction
 	}
 	return nil
 }
@@ -206,6 +385,12 @@ var file_contracts_finance_accounts_v1_transaction_proto_rawDesc = []byte{
 	0x2e, 0x66, 0x69, 0x6e, 0x61, 0x6e, 0x63, 0x65, 0x2e, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63,
 	0x74, 0x69, 0x6f, 0x6e, 0x52, 0x0b, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f,
 	0x6e, 0x22, 0x1b, 0x0a, 0x19, 0x43, 0x72, 0x65, 0x61, 0x74, 0x65, 0x54, 0x72, 0x61, 0x6e, 0x73,
+	0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x41,
+	0x0a, 0x18, 0x44, 0x65, 0x6c, 0x65, 0x74, 0x65, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74,
+	0x69, 0x6f, 0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x25, 0x0a, 0x0e, 0x74, 0x72,
+	0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x0d, 0x52, 0x0d, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x49,
+	0x64, 0x22, 0x1b, 0x0a, 0x19, 0x44, 0x65, 0x6c, 0x65, 0x74, 0x65, 0x54, 0x72, 0x61, 0x6e, 0x73,
 	0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x19,
 	0x0a, 0x17, 0x4c, 0x69, 0x73, 0x74, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f,
 	0x6e, 0x73, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x22, 0x54, 0x0a, 0x18, 0x4c, 0x69, 0x73,
@@ -213,12 +398,23 @@ var file_contracts_finance_accounts_v1_transaction_proto_rawDesc = []byte{
 	0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x38, 0x0a, 0x0c, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63,
 	0x74, 0x69, 0x6f, 0x6e, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x14, 0x2e, 0x66, 0x69,
 	0x6e, 0x61, 0x6e, 0x63, 0x65, 0x2e, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f,
-	0x6e, 0x52, 0x0c, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x42,
-	0x40, 0x5a, 0x3e, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x62, 0x61,
-	0x72, 0x2d, 0x72, 0x61, 0x69, 0x73, 0x65, 0x72, 0x73, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x6e, 0x74,
-	0x69, 0x6f, 0x6e, 0x2f, 0x63, 0x6f, 0x6e, 0x74, 0x72, 0x61, 0x63, 0x74, 0x73, 0x2f, 0x66, 0x69,
-	0x6e, 0x61, 0x6e, 0x63, 0x65, 0x2f, 0x61, 0x63, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x73, 0x2f, 0x76,
-	0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x6e, 0x52, 0x0c, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x73, 0x22,
+	0x52, 0x0a, 0x18, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63,
+	0x74, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x36, 0x0a, 0x0b, 0x74,
+	0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b,
+	0x32, 0x14, 0x2e, 0x66, 0x69, 0x6e, 0x61, 0x6e, 0x63, 0x65, 0x2e, 0x54, 0x72, 0x61, 0x6e, 0x73,
+	0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x0b, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74,
+	0x69, 0x6f, 0x6e, 0x22, 0x53, 0x0a, 0x19, 0x55, 0x70, 0x64, 0x61, 0x74, 0x65, 0x54, 0x72, 0x61,
+	0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
+	0x12, 0x36, 0x0a, 0x0b, 0x74, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x14, 0x2e, 0x66, 0x69, 0x6e, 0x61, 0x6e, 0x63, 0x65, 0x2e,
+	0x54, 0x72, 0x61, 0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x52, 0x0b, 0x74, 0x72, 0x61,
+	0x6e, 0x73, 0x61, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x42, 0x40, 0x5a, 0x3e, 0x67, 0x69, 0x74, 0x68,
+	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x62, 0x61, 0x72, 0x2d, 0x72, 0x61, 0x69, 0x73, 0x65,
+	0x72, 0x73, 0x2f, 0x69, 0x6e, 0x74, 0x65, 0x6e, 0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x63, 0x6f, 0x6e,
+	0x74, 0x72, 0x61, 0x63, 0x74, 0x73, 0x2f, 0x66, 0x69, 0x6e, 0x61, 0x6e, 0x63, 0x65, 0x2f, 0x61,
+	0x63, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x73, 0x2f, 0x76, 0x31, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
+	0x6f, 0x33,
 }
 
 var (
@@ -233,22 +429,28 @@ func file_contracts_finance_accounts_v1_transaction_proto_rawDescGZIP() []byte {
 	return file_contracts_finance_accounts_v1_transaction_proto_rawDescData
 }
 
-var file_contracts_finance_accounts_v1_transaction_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_contracts_finance_accounts_v1_transaction_proto_msgTypes = make([]protoimpl.MessageInfo, 8)
 var file_contracts_finance_accounts_v1_transaction_proto_goTypes = []interface{}{
 	(*CreateTransactionRequest)(nil),  // 0: accounts.CreateTransactionRequest
 	(*CreateTransactionResponse)(nil), // 1: accounts.CreateTransactionResponse
-	(*ListTransactionsRequest)(nil),   // 2: accounts.ListTransactionsRequest
-	(*ListTransactionsResponse)(nil),  // 3: accounts.ListTransactionsResponse
-	(*finance.Transaction)(nil),       // 4: finance.Transaction
+	(*DeleteTransactionRequest)(nil),  // 2: accounts.DeleteTransactionRequest
+	(*DeleteTransactionResponse)(nil), // 3: accounts.DeleteTransactionResponse
+	(*ListTransactionsRequest)(nil),   // 4: accounts.ListTransactionsRequest
+	(*ListTransactionsResponse)(nil),  // 5: accounts.ListTransactionsResponse
+	(*UpdateTransactionRequest)(nil),  // 6: accounts.UpdateTransactionRequest
+	(*UpdateTransactionResponse)(nil), // 7: accounts.UpdateTransactionResponse
+	(*finance.Transaction)(nil),       // 8: finance.Transaction
 }
 var file_contracts_finance_accounts_v1_transaction_proto_depIdxs = []int32{
-	4, // 0: accounts.CreateTransactionRequest.transaction:type_name -> finance.Transaction
-	4, // 1: accounts.ListTransactionsResponse.transactions:type_name -> finance.Transaction
-	2, // [2:2] is the sub-list for method output_type
-	2, // [2:2] is the sub-list for method input_type
-	2, // [2:2] is the sub-list for extension type_name
-	2, // [2:2] is the sub-list for extension extendee
-	0, // [0:2] is the sub-list for field type_name
+	8, // 0: accounts.CreateTransactionRequest.transaction:type_name -> finance.Transaction
+	8, // 1: accounts.ListTransactionsResponse.transactions:type_name -> finance.Transaction
+	8, // 2: accounts.UpdateTransactionRequest.transaction:type_name -> finance.Transaction
+	8, // 3: accounts.UpdateTransactionResponse.transaction:type_name -> finance.Transaction
+	4, // [4:4] is the sub-list for method output_type
+	4, // [4:4] is the sub-list for method input_type
+	4, // [4:4] is the sub-list for extension type_name
+	4, // [4:4] is the sub-list for extension extendee
+	0, // [0:4] is the sub-list for field type_name
 }
 
 func init() { file_contracts_finance_accounts_v1_transaction_proto_init() }
@@ -282,7 +484,7 @@ func file_contracts_finance_accounts_v1_transaction_proto_init() {
 			}
 		}
 		file_contracts_finance_accounts_v1_transaction_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*ListTransactionsRequest); i {
+			switch v := v.(*DeleteTransactionRequest); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -294,7 +496,55 @@ func file_contracts_finance_accounts_v1_transaction_proto_init() {
 			}
 		}
 		file_contracts_finance_accounts_v1_transaction_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*DeleteTransactionResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_contracts_finance_accounts_v1_transaction_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*ListTransactionsRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_contracts_finance_accounts_v1_transaction_proto_msgTypes[5].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*ListTransactionsResponse); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_contracts_finance_accounts_v1_transaction_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateTransactionRequest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_contracts_finance_accounts_v1_transaction_proto_msgTypes[7].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*UpdateTransactionResponse); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -312,7 +562,7 @@ func file_contracts_finance_accounts_v1_transaction_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_contracts_finance_accounts_v1_transaction_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   4,
+			NumMessages:   8,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
